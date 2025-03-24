c32ba60a55b8a4faf49a29a7fb6d75a061fcf226
orang3i <orangeinfotainment@gmail.com>
2025-03-24 18:30:30 +0530

    chore: normalize line endings to LF

---
+++ docs/.vitepress/themes/index.ts
@@ -0,0 +1,9 @@
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+import type { Theme } from 'vitepress'
+
+const customTheme: Theme = {
+  ...DefaultTheme
+}
+
+export default customTheme
