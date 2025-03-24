995e4b00ea3b795327570f74154c44cc992f7eb6
orang3i <orangeinfotainment@gmail.com>
2025-03-24 17:53:36 +0530

    ssssssss

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.vuejs.org/config/app-configs
+export default defineConfig({
+    title: "Lumen",
+    description: "Documentation for Lumen",
+    base: "/LumenWeb/", // <-- Important for GitHub Pages!
+})
